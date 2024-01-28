90a031d83437213411d953f774d92e4f1bbf15fb
weiru00 <88423416+weiru00@users.noreply.github.com>
2024-01-28 15:05:00 +0800

    Add files via upload

--- smarthome/smarthome.ino
+++ smarthome/smarthome.ino
@@ -4,6 +4,7 @@ ESP32 publish telemetry data to Google Cloud (DHT11 sensor)
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "DHT.h"
+#include <ESP32Servo.h>
 
 const char *WIFI_SSID = "MH"; //your WiFi SSID
 const char *WIFI_PASSWORD = "abcde123"; // your password
@@ -16,9 +17,31 @@ const int tempPin = A4;        // LEFT SIDE MAKER PORT
 const int rainPin = A2;       // MIDDLE MAKER PORT
 const int IRPin = 42;     // RIGHT SIDE MAKER PORT
 
+// DEFINE OUTPUT PIN
+const int ledNbuzzer = 21; // FOR HOME SECURITY
+const int light = 48;
+const int fan = 47;
+const int servoPin = 40;
+
 // FOR DHT SENSOR
 #define DHTTYPE DHT11
 DHT dht(tempPin, DHTTYPE);
+
+//  FOR SERVO
+Servo Myservo;
+
+// FOR RAIN SENSOR
+int MinDepthValue = 4095;
+int MaxDepthValue = 2170;
+int MinDepth = 0;
+int MaxDepth = 100;
+int depth = 0;
+const int rainThreshold = 50; // Set this according to your needs
+const int shadingPosition = 180; // Set this to the angle you want for shading
+
+// FOR IR SENSOR
+unsigned long lastTrigger = 0;
+boolean obstacle = false;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -47,6 +70,18 @@ void setup()
   pinMode(rainPin, INPUT);
   pinMode(tempPin, INPUT);
   pinMode(IRPin, INPUT);
+
+  // actuator
+  Myservo.attach(servoPin);
+  Myservo.write(0);
+  pinMode(ledNbuzzer, OUTPUT);
+  pinMode(light, OUTPUT);
+  pinMode(fan, OUTPUT);
+
+  // SET INITAL OUTPUT TO LOW
+  digitalWrite(ledNbuzzer, LOW);
+  digitalWrite(light, LOW);
+  digitalWrite(fan, LOW);
 
   setup_wifi();
   client.setServer(MQTT_SERVER, MQTT_PORT);
@@ -85,13 +120,37 @@ void loop()
   char payload[50];
   sprintf(payload, "Temperature: %.2f, Humidity: %.2f", temperature, humidity);
   client.publish(MQTT_TOPIC, payload);
+  if (temperature > 31) {
+    Serial.println("Temperature is above 31C. Turning fan ON.");
+    digitalWrite(fan, HIGH); // Turn the fan on
+  } else {
+    Serial.println("Temperature is below 31C. Turning fan OFF.");
+    digitalWrite(fan, LOW); // Turn the fan off
+  }
 
-  float rain = analogRead(rainPin);
+  // Read rain sensor data
+  int rain = analogRead(rainPin);
   sprintf(payload, "Rain Depth: %.2f", rain);
   client.publish(MQTT_TOPIC, payload);
+  depth = map(rain, MinDepthValue, MaxDepthValue, MinDepth, MaxDepth);
+  // Activate shading when rain is detected
+  if (depth > rainThreshold) {
+      Myservo.write(shadingPosition); // Move servo to shading position
+  } else {
+      Myservo.write(0); // Reset servo position when there's no rain
+  }
 
-  int obstacle = digitalRead(IRPin);
+  // Handle motion detection by IR Sensor
+  int obstacle = !digitalRead(IRPin);
   sprintf(payload, "Obstacle: %d", obstacle);
   client.publish(MQTT_TOPIC, payload);
+  //  Activate LED and buzzer when motion detected
+  if (obstacle == HIGH) {
+      // Turn on LED and buzzer
+      digitalWrite(ledNbuzzer, HIGH);
+  }
+  else {
+    digitalWrite(ledNbuzzer, LOW);
+  }
 
 }
